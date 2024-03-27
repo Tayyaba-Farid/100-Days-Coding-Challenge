ad1706701b17a1a2db91b7205d3c46f6f874731f
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-27 23:47:04 +0500

    Assignment 47

---
+++ Assignment 47/file.js
@@ -0,0 +1,7 @@
+var laptop = [
+    { make: "Apple", model: "MacBook Pro", year: 2023 },
+    { make: "Dell", model: "XPS 13", year: 2022 },
+    { make: "HP", model: " Spectre x360", year: 2024 }
+];
+var laptop1 = laptop[0], laptop2 = laptop[1], laptop3 = laptop[2];
+console.log(laptop2, laptop1);
---
+++ Assignment 47/file.ts
@@ -0,0 +1,8 @@
+let laptop = [
+    {make: "Apple", model: "MacBook Pro", year: 2023},
+    {make: "Dell", model: "XPS 13", year: 2022},
+    {make: "HP", model:" Spectre x360" , year: 2024}
+]
+
+let [laptop1, laptop2, laptop3] = laptop;
+console.log(laptop2, laptop1)
