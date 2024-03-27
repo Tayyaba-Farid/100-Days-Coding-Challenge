6ad290171b98879fda899ba0059a261a6bd412b0
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-27 23:47:52 +0500

    Assignment 50

---
+++ Assignment 50/file.js
@@ -0,0 +1,3 @@
+// in multiline template literals, you use backticks.
+var myDay = "My ideal day is:\nI love watching netflix\nAnd then I love coding\nAnd then I eat\nThen I love going for jogging";
+console.log(myDay);
---
+++ Assignment 50/file.ts
@@ -0,0 +1,10 @@
+// in multiline template literals, you use backticks.
+
+let myDay = 
+`My ideal day is:
+I love watching netflix
+And then I love coding
+And then I eat
+Then I love going for jogging`
+
+console.log(myDay)
