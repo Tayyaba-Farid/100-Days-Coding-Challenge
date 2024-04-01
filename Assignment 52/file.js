3c18c48616bf4ed75ec688f12c2a2f5e7a536435
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 22:57:38 +0500

    Assignment 52

---
+++ Assignment 52/file.js
@@ -0,0 +1,10 @@
+var smartphone = {
+    Brand: "Samsung",
+    Model: "Galaxy S21 Ultra",
+    otherkeyeatures: {
+        Storage: " 512GB",
+        ScreenSize: 6.8,
+        BatteryLife: "Up to 24 hours of talk time"
+    }
+};
+console.log(smartphone);
---
+++ Assignment 52/file.ts
@@ -0,0 +1,12 @@
+
+
+let smartphone = {
+    Brand: "Samsung",
+    Model: "Galaxy S21 Ultra",
+    otherkeyeatures :  {
+    Storage: " 512GB",
+    ScreenSize: 6.8,
+    BatteryLife: "Up to 24 hours of talk time"
+}
+}
+console.log(smartphone)
