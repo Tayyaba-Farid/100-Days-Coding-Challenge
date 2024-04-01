5ec19c9ccf357dcc82cf225c80d3306fe40e2403
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:02:40 +0500

    Assignment 71

---
+++ Assignment 71/file.js
@@ -0,0 +1,10 @@
+var myname = "tayyaba";
+myname = "Wania";
+console.log(myname);
+var age = 23;
+try {
+    age = 25;
+}
+catch (error) {
+    console.log("Error: it is giving error because we cannot reassigned value to const");
+}
---
+++ Assignment 71/file.ts
@@ -0,0 +1,11 @@
+let myname: string = "tayyaba";
+myname = "Wania";
+console.log(myname)
+
+const age: number = 23;
+try{
+    age = 25; 
+}catch(error) {
+console.log("Error: it is giving error because we cannot reassigned value to const")
+}
+
