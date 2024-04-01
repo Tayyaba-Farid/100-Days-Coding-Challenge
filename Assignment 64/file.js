9c1657b86cf85bcfb7ab056a7de9445bcefb586f
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:00:55 +0500

    Assignment 64

---
+++ Assignment 64/file.js
@@ -0,0 +1,4 @@
+function combinetextiwthnumber(text, number) {
+    return text + number;
+}
+console.log(combinetextiwthnumber("Marks:", 95));
---
+++ Assignment 64/file.ts
@@ -0,0 +1,5 @@
+function combinetextiwthnumber(text: string, number: number): string{
+return text + number
+}
+
+console.log(combinetextiwthnumber("Marks:", 95))
