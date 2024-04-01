83028f5b14a98548d5170efcbdb37cf0fb1e424b
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:01:06 +0500

    Assignment 65

---
+++ Assignment 65/file.js
@@ -0,0 +1,4 @@
+function getModulus(num1, num2) {
+    return num1 % num2;
+}
+console.log(getModulus(9, 2));
---
+++ Assignment 65/file.ts
@@ -0,0 +1,5 @@
+function getModulus(num1: number, num2: number): number{
+    return num1 % num2
+}
+
+console.log(getModulus(9, 2))
