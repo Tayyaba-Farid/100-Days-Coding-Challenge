add5ea48c9e8f9c0228bbba0e3d89253c4ed199a
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:01:33 +0500

    Assignment 67

---
+++ Assignment 67/file.js
@@ -0,0 +1,4 @@
+function addstringwithnumber(numberString, number) {
+    return numberString + number;
+}
+console.log(addstringwithnumber("10", 5));
---
+++ Assignment 67/file.ts
@@ -0,0 +1,5 @@
+function addstringwithnumber(numberString: string, number:number){
+    return numberString + number
+}
+
+console.log(addstringwithnumber("10", 5))
