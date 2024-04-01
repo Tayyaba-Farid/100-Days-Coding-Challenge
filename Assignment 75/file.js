64202af89e788bc986f1eb96d066ef3b1ed771ac
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:03:27 +0500

    Assignment 75

---
+++ Assignment 75/file.js
@@ -0,0 +1,13 @@
+function compoundoperators() {
+    var x = 4;
+    console.log("value of x: ", x);
+    x += 2;
+    console.log("value of x: ", x);
+    x -= 3;
+    console.log("value of x: ", x);
+    x *= 4;
+    console.log("value of x: ", x);
+    x /= 2;
+    console.log("value of x: ", x);
+}
+compoundoperators();
---
+++ Assignment 75/file.ts
@@ -0,0 +1,16 @@
+function compoundoperators() {
+  let x = 4;
+  console.log("value of x: ", x);
+  x += 2;
+  console.log("value of x: ", x);
+
+  x -= 3;
+  console.log("value of x: ", x);
+
+  x *= 4;
+  console.log("value of x: ", x);
+
+  x /= 2;
+  console.log("value of x: ", x);
+}
+compoundoperators()
