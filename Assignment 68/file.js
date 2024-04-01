ab08279d588db5fafbdbfaa2e03177553a4c22f7
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:01:45 +0500

    Assignment 68

---
+++ Assignment 68/file.js
@@ -0,0 +1,4 @@
+function decimalproduct(num1, num2) {
+    return ((num1 * num2) * 100) / 100;
+}
+console.log(decimalproduct(0.34, 0.56));
---
+++ Assignment 68/file.ts
@@ -0,0 +1,5 @@
+function decimalproduct(num1: number, num2: number){
+    return ((num1 * num2) * 100) /100
+}
+
+console.log(decimalproduct(0.34, 0.56))
