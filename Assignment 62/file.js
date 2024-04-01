d365f01671374cb2cb18a8294dc88c74cbdf61f0
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:00:33 +0500

    Assignment 62

---
+++ Assignment 62/file.js
@@ -0,0 +1,6 @@
+var student = {
+    name: "Ayra",
+    age: 22,
+    classes: ["Maths", "Urdu", "English", "Science"]
+};
+console.log(student);
---
+++ Assignment 62/file.ts
@@ -0,0 +1,13 @@
+interface studentInfo {
+    name: string,
+    age: number,
+    classes: string[]
+}
+
+let student: studentInfo = {
+    name: "Ayra",
+    age: 22,
+    classes: ["Maths", "Urdu", "English", "Science"]
+}
+
+console.log(student)
