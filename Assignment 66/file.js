1b1463fc9a075f8504183dad476a8a64969255a1
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-01 23:01:16 +0500

    Assignment 65

---
+++ Assignment 66/file.js
@@ -0,0 +1,4 @@
+function checkboolean(text1, text2) {
+    return text1 && text2;
+}
+console.log(checkboolean(true, true));
---
+++ Assignment 66/file.ts
@@ -0,0 +1,4 @@
+function checkboolean(text1: boolean, text2: boolean): boolean{
+    return text1 && text2
+}
+console.log(checkboolean(true, false))
