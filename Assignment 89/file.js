c1ebe8fe4ad26a8be013b8c7310f584aadb10742
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:53:53 +0500

    Assignment 89

---
+++ Assignment 89/file.js
@@ -0,0 +1,4 @@
+function strintonumber(text) {
+    return parseInt(text);
+}
+console.log(strintonumber("123456"));
---
+++ Assignment 89/file.ts
@@ -0,0 +1,4 @@
+function strintonumber(text: string): number {
+    return parseInt(text)
+}
+console.log(strintonumber("123456"))
