48a676e8787b60218766ee0654668161321edc6d
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:51:24 +0500

    Assignment 78

---
+++ Assignment 78/file.js
@@ -0,0 +1,8 @@
+function squareexpression(num1, num2) {
+    return num1 * num2;
+}
+var a = function (num1, num2) {
+    return num1 * num2;
+};
+console.log(squareexpression(2, 10));
+console.log(a(2, 20));
---
+++ Assignment 78/file.ts
@@ -0,0 +1,9 @@
+function squareexpression(num1: number, num2: number){
+    return num1 * num2
+}
+
+let a = function squaredeclaration(num1: number, num2: number){
+    return num1 * num2
+}
+console.log(squareexpression(2, 10))
+console.log(a(2, 20))
