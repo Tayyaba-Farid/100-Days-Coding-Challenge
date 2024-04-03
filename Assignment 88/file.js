e46ecaa82581f43e6cee595aa15cae91ddc24061
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:53:40 +0500

    Assignment 88

---
+++ Assignment 88/file.js
@@ -0,0 +1,4 @@
+function roundinteger(num) {
+    return Math.round(num);
+}
+console.log(roundinteger(2.4));
---
+++ Assignment 88/file.ts
@@ -0,0 +1,4 @@
+function roundinteger(num: number): number {
+    return Math.round(num)
+}
+console.log(roundinteger(2.4))
