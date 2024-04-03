bb5080b538ae2b092ab69a6afc143098f3e288c9
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-04 01:57:01 +0500

    Assignment 94

---
+++ Assignment 94/file.js
@@ -0,0 +1,3 @@
+var countlength = ["paris", "tokyo", "istanbul", "islamabad", "venice"];
+var newarray = countlength.map(function (number) { return number.length; });
+console.log(newarray);
---
+++ Assignment 94/file.ts
@@ -0,0 +1,3 @@
+let countlength: string[] = ["paris", "tokyo", "istanbul", "islamabad", "venice"]
+let newarray: number[] = countlength.map(number => number.length)
+console.log(newarray)
