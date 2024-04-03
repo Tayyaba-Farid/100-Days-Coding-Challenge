fa928c7be6b6e63ef838babddf6f9819fe00df0a
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-04 01:57:42 +0500

    Assignment 97

---
+++ Assignment 97/file.ts
@@ -0,0 +1,14 @@
+function getdateFormatted(): string{
+let now = new Date();
+let day = String(now.getDate()).padStart(2, '0');
+let month = String(now.getMonth() + 1).padStart(2,'0');
+let year = now.getFullYear();
+return (`${day}-${month}-${year}`)
+}
+console.log(getdateFormatted())
+
+
+
+
+
+
