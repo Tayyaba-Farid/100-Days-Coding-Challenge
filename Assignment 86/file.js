0cc34d3ede64152cf82a9155d640d4b67eae5f89
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:53:20 +0500

    Assignment 86

---
+++ Assignment 86/file.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function findtext(text) {
+    return text.includes("javascript");
+}
+console.log(findtext("I am doing a course of javascript and typescript"));
---
+++ Assignment 86/file.ts
@@ -0,0 +1,4 @@
+function findtext(text: string): boolean{
+    return text.includes("javascript")
+}
+console.log(findtext("I am doing a course of javascript and typescript"))
