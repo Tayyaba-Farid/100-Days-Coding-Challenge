d223bd16e62397391bf21391695e1b0362aabbd5
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-04 01:56:24 +0500

    Assignment 91

---
+++ Assignment 91/file.js
@@ -0,0 +1,3 @@
+var fruits = ["melon", "mango", "strawberry"];
+fruits.push("pomegranate");
+console.log(fruits);
---
+++ Assignment 91/file.ts
@@ -0,0 +1,3 @@
+let fruits = ["melon", "mango", "strawberry"]
+fruits.push("pomegranate")
+console.log(fruits)
