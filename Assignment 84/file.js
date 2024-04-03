7b995eac5138172022e31478d659829a8075bc3e
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:52:47 +0500

    Assignment 84

---
+++ Assignment 84/file.js
@@ -0,0 +1,8 @@
+// function changeword(sent: string){
+//     return sent.replace(/Javascript/g, "Typescript")
+// }
+// console.log(changeword("We will learn Javascript in this Javascript course"))
+function replacesent(text) {
+    return text.replace(/we are going to do this/g, "we will gonna do this");
+}
+console.log(replacesent("we are going to do this tomorrow"));
---
+++ Assignment 84/file.ts
@@ -0,0 +1,4 @@
+function changeword(sent: string){
+    return sent.replace(/Javascript/g, "Typescript")
+}
+console.log(changeword("We will learn Javascript in this Javascript course"))
