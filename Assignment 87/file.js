eb0e2680b75dc192877101517b427f126294e5ad
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:53:30 +0500

    Assignment 87

---
+++ Assignment 87/file.js
@@ -0,0 +1,4 @@
+function extractstring(text) {
+    return text.substring(0, 10);
+}
+console.log(extractstring("This course is a 1 year program"));
---
+++ Assignment 87/file.ts
@@ -0,0 +1,4 @@
+function extractstring(text: string): string{
+    return text.substring(0, 10)
+}
+console.log(extractstring("This course is a 1 year program"))
