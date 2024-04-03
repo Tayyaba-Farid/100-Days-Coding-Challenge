0932347264638cf899c0af9a1b4f06adc5c148ff
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-03 23:52:32 +0500

    Assignment 83

---
+++ Assignment 83/file.js
@@ -0,0 +1,6 @@
+function changestring(text) {
+    var upper = text.toUpperCase();
+    var lower = text.toLowerCase();
+    console.log(upper, lower);
+}
+console.log(changestring("Tayyaba"));
---
+++ Assignment 83/file.ts
@@ -0,0 +1,6 @@
+function changestring(text: string){
+ let upper: string = text.toUpperCase();
+ let lower: string =  text.toLowerCase()
+ console.log(upper, lower)
+}
+console.log(changestring("Tayyaba"))
