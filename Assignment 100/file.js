c9d7b56cc0ed8bc04040a404688326f349775d89
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-04 20:21:12 +0500

    Assignment 100

---
+++ Assignment 100/file.js
@@ -0,0 +1,4 @@
+var squareRoot = Math.sqrt(144);
+console.log(squareRoot);
+var num2 = Math.sqrt(72864);
+console.log(num2);
---
+++ Assignment 100/file.ts
@@ -0,0 +1,5 @@
+let squareRoot: number = Math.sqrt(144);
+console.log(squareRoot)
+
+let num2: number = Math.sqrt(72864)
+console.log(num2)
