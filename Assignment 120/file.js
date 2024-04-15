38f07e3b2971da03ebe951a7eef63a1ac513802c
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:02:58 +0500

    Assignment 120

---
+++ Assignment 120/file.js
@@ -0,0 +1,5 @@
+var favMovies = ["The Godfather", "Star Wars", "The Shawshank Redemption", "The Lord of the Rings", "Titanic"];
+for (var _i = 0, favMovies_1 = favMovies; _i < favMovies_1.length; _i++) {
+    var movie = favMovies_1[_i];
+    console.log(movie);
+}
---
+++ Assignment 120/file.ts
@@ -0,0 +1,4 @@
+let favMovies: string[] = ["The Godfather", "Star Wars", "The Shawshank Redemption", "The Lord of the Rings", "Titanic"]
+for (let movie of favMovies){
+    console.log(movie)
+}
