ea1506375487da9e18500b446af7adb856c9f5b7
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:02:17 +0500

    Assignment 117

---
+++ Assignment 117/file.ts
@@ -0,0 +1,23 @@
+function evaluateGrades(marks: string): void {
+  switch (marks) {
+    case "A":
+      console.log("Excellent");
+      break;
+    case "B":
+      console.log("Good");
+      break;
+    case "C":
+      console.log("Fair");
+      break;
+    case "D":
+      console.log("Average");
+      break;
+    case "F":
+      console.log("Bad result");
+      break;
+    default:
+      console.log("Invalid grade");
+      break;
+  }
+}
+evaluateGrades("B");
