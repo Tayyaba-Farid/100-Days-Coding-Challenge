94b359cb5bc3198333ea95d97144171e494608a2
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:00:23 +0500

    Assignment 108

---
+++ Assignment 108/file.js
@@ -0,0 +1,4 @@
+function compareString(text1, text2) {
+    return text1.toLowerCase() === text2.toLowerCase();
+}
+console.log(compareString("hello", "HeLlo"));
---
+++ Assignment 108/file.ts
@@ -0,0 +1,4 @@
+function compareString(text1: string, text2: string): boolean{
+    return text1.toLowerCase() === text2.toLowerCase()
+}
+console.log(compareString("hello", "HeLlo"))
