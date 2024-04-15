67a16cd7cf4bde74f10ded0604e50a07c2f0b2b9
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:01:05 +0500

    Assignment 112

---
+++ Assignment 112/file.js
@@ -0,0 +1,6 @@
+let countries = new Map();
+countries.set("Canada", "Ottawa");
+countries.set("France", "Paris");
+countries.set("Pakistan", "Islamabad");
+console.log(countries);
+export {};
---
+++ Assignment 112/file.ts
@@ -0,0 +1,5 @@
+let countries = new Map<string, string>();
+countries.set("Canada", "Ottawa")
+countries.set("France", "Paris")
+countries.set("Pakistan", "Islamabad")
+console.log(countries)
