9766441a2fa436f4428a48c7d73b1acc96b4a7f8
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:00:11 +0500

    Assignment 107

---
+++ Assignment 107/file.js
@@ -0,0 +1,4 @@
+function checkDivisible(num) {
+    return num % 2 === 0 && num % 3 === 0;
+}
+console.log(checkDivisible(15));
---
+++ Assignment 107/file.ts
@@ -0,0 +1,4 @@
+function checkDivisible(num: number): boolean{
+return num % 2 === 0 && num % 3 === 0;
+    }
+    console.log(checkDivisible(12))
