0dbcc7987ef4a074dd5e1be05c313d2931bab904
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:00:44 +0500

    Assignment 110

---
+++ Assignment 110/file.ts
@@ -0,0 +1,23 @@
+function assignGrades(grades: number): string{
+if(grades <= 100 && grades >= 90){
+   return "A"
+}
+else if(grades <= 90 && grades >= 80){
+    return "A-"
+}
+else if(grades <= 80 && grades >= 70){
+    return "B"
+}
+else if(grades <= 70 && grades >= 60){
+    return "C"
+}
+else if(grades <= 60 && grades >= 50){
+    return "D"
+}
+else{
+    return "FAIL"
+}
+}
+console.log(assignGrades(84))
+console.log(assignGrades(34))
+console.log(assignGrades(65))
