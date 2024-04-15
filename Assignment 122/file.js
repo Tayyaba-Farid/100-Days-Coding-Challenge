ed22776ee85eb994bf6c233fd2251f2ffe7b652a
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 19:43:03 +0500

    Assignment 122

---
+++ Assignment 122/file.js
@@ -0,0 +1,8 @@
+var numbers = 10;
+while (numbers > 0) {
+    if (numbers === 5) {
+        break;
+    }
+    console.log(numbers);
+    numbers--;
+}
---
+++ Assignment 122/file.ts
@@ -0,0 +1,9 @@
+let numbers: number = 10;
+while (numbers > 0) {
+  if (numbers === 5) {
+    break;
+  }
+
+  console.log(numbers);
+  numbers--;
+}
