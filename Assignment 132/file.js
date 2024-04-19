13daab16ae4c5a40ab98fe5aac97423168ea328e
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-19 16:54:47 +0500

    Assignment 132

---
+++ Assignment 132/file.js
@@ -0,0 +1,8 @@
+var person = {
+    name: "Tayyaba",
+    age: 23,
+    isActive: true,
+    id: 34565
+};
+var jsonString = JSON.stringify(person);
+console.log(jsonString);
---
+++ Assignment 132/file.ts
@@ -0,0 +1,9 @@
+let person = {
+    name: "Tayyaba",
+    age: 23,
+    isActive: true,
+    id: 34565
+}
+
+let jsonString = JSON.stringify(person)
+console.log(jsonString)
