5fbade5c4998e0831eba2b97b8ffdf3ff2cb0f0d
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-19 16:55:07 +0500

    Assignment 134

---
+++ Assignment 134/file.js
@@ -0,0 +1,3 @@
+var jsonString = "{\n    \"name\": \"Tayyaba\",\n    \"age\": 23,\n    \"city\": \"karachi\",\n    \"country\": \"pakistan\" }";
+var convertIntoObject = JSON.parse(jsonString);
+console.log(convertIntoObject);
---
+++ Assignment 134/file.ts
@@ -0,0 +1,8 @@
+let jsonString = `{
+    "name": "Tayyaba",
+    "age": 23,
+    "city": "karachi",
+    "country": "pakistan" }`
+
+let convertIntoObject = JSON.parse(jsonString)
+console.log(convertIntoObject)
