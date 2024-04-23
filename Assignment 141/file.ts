bf6a2b8c6bf0b39171f844fea6fdf073875ee94c
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-23 17:00:40 +0500

    Assignment 141

---
+++ Assignment 141/file.ts
@@ -0,0 +1,7 @@
+// significance of await keyword
+
+async function fetchData() {
+    let data = await fetchData()
+    console.log(data)
+}
+console.log("the await operator helps asynchronous, promise based behaviour to be written as a cleaner code.")
