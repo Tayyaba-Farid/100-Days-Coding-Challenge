50d469a91d475ba7c55f0570051d3f3bba5e11a5
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-24 17:53:32 +0500

    Assignment 149

---
+++ Assignment 149/file.js
@@ -0,0 +1,4 @@
+// 148th assigment
+setTimeout(function () {
+    console.log("We are demoonstrating setTimeOut function");
+}, 1000);
---
+++ Assignment 149/file.ts
@@ -0,0 +1,5 @@
+// 148th assigment
+
+setTimeout(() => {
+  console.log("We are demoonstrating setTimeOut function");
+}, 1000);
