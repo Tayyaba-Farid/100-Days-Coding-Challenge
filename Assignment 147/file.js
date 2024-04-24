b9aa15ba6d3b35d8764ac33f8f6b1db95684c0f3
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-24 17:53:12 +0500

    Assignment 147

---
+++ Assignment 147/file.js
@@ -0,0 +1,3 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var filterArray = arr.filter(function (number) { return number < 5; });
+console.log(filterArray);
---
+++ Assignment 147/file.ts
@@ -0,0 +1,5 @@
+let arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let filterArray: number[] = arr.filter((number) => number < 5)
+
+console.log(filterArray)
